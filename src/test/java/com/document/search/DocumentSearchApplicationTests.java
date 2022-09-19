d7a23f02e6cd4ee9d291d7d085f1eea3e24daf92
ptiwari <ptiwari@clearwateranalytics.com>
2022-09-19 10:54:36 +0530

    Initial Code Changes

---
+++ src/test/java/com/document/search/DocumentSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
